e0f520069523e62c9089130456f413445c873328
Tritti2023 <reto.trittibach@edu.teko.ch>
2023-09-14 18:59:08 +0200

    Erste Versuche Frontend

--- client/script.js
+++ client/script.js
@@ -31,9 +31,11 @@ socket.addEventListener("message", (event) => {
       socket.send(JSON.stringify({ type: "pong", data: "FROM CLIENT" }));
     case "user":
       // TODO: Show the current users as DOM elements
+      showUsers(messageObject.users)
       break;
     case "message":
       // TODO: Show new message as DOM element append to chat history
+      showMessage(messageObject.message)
       break;
     default:
       console.error("Unknown message type: " + messageObject.type);
@@ -42,10 +44,48 @@ socket.addEventListener("message", (event) => {
 
 function showUsers(users) {
   // TODO: Show the current users as DOM elements
+  const usersElement = document.getElementById("users");
+  usersElement.innerHTML = "";
+  users.forEach((user) => {
+    const userElement = document.createElement("div");
+    userElement.innerHTML = "🟢 " + user.name;
+    usersElement.appendChild(userElement);
+  });
 }
 
 function showMessage(message) {
   // TODO: Show new message as DOM element append to chat history
+  const messageElement = document.createElement("div");
+  const innerMessageElement = document.createElement("div");
+  const headerElement = document.createElement("span");
+  const usernameElement = document.createElement("span");
+  const timeElement = document.createElement("span");
+  const messageTextElement = document.createElement("p");
+  innerMessageElement.classList.add(
+    "rounded-lg",
+    "p-4",
+    "inline-flex",
+    "flex-col",
+    "gap-2"
+  );
+  headerElement.classList.add("font-bold", "flex", "gap-2", "items-center");
+  if (message.user.id === userId) {
+    messageElement.classList.add("text-right", "self-end");
+    innerMessageElement.classList.add("bg-green-500", "border-2", "border-green-400");
+  } else {
+    innerMessageElement.classList.add("bg-slate-500", "border-2", "border-gray-400");
+  }
+  usernameElement.innerHTML = message.user.name;
+  timeElement.innerHTML = "at " + message.time;
+  timeElement.classList.add("text-xs");
+  headerElement.appendChild(usernameElement);
+  headerElement.appendChild(timeElement);
+  messageTextElement.innerHTML = message.message;
+  innerMessageElement.appendChild(headerElement);
+  innerMessageElement.appendChild(messageTextElement);
+  messageElement.appendChild(innerMessageElement);
+  document.getElementById("messages").appendChild(messageElement);
+  messageElement.scrollIntoView();
 }
 
 socket.addEventListener("close", (event) => {
@@ -58,10 +98,34 @@ socket.addEventListener("error", (event) => {
 
 function changeUsername() {
   // TODO: Implement change username and forward new username to backend
+  const newUsername = document.getElementById("username").value;
+  if (newUsername === "") return;
+  const message = {
+    type: "user",
+    user: {
+      id: userId,
+      name: document.getElementById("username").value,
+    },
+  };
   socket.send(JSON.stringify(message));
 }
 
 function sendMessage() {
   // TODO get message from input and send message as object to backend
+  const messageText = document.getElementById("message").value;
+  if (messageText === "") return;
+  const message = {
+    type: "message",
+    message: {
+      user: {
+        id: userId,
+        name: document.getElementById("username").value,
+      },
+      message: messageText,
+      time: new Date().toLocaleTimeString(),
+    },
+  }
   socket.send(JSON.stringify(message));
+  document.getElementById("message").value = "";
+
 }
